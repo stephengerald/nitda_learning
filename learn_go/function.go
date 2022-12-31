642ebf649440f244898d4d84dff8ce96bfe1a1e7
STERUX <stephenokpala11@gmail.com>
2022-12-31 15:34:47 +0100

    new programming language

---
+++ learn_go/function.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+/* func main() {
+	fmt.Printf("hello world\n")
+} */
+
+func hello(name string) string {
+	message := fmt.Sprintf("hello %v, welcome!.\n", name)
+	return message
+}
+
+func main() {
+	fmt.Printf(hello("Stephen"))
+}
